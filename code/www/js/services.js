e1318befd91db277b1aa8c93da1e2099c020fab4
Michael Gifford <michaelcgifford@gmail.com>
2016-08-11 01:05:24 -0500

    Storing session data in local storage

--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,5 +1,5 @@
-angular.module('songhop.services', [])
-.factory('User', function($http, SERVER) {
+angular.module('songhop.services', ['ionic.utils'])
+.factory('User', function($http, $q, $localstorage, SERVER) {
 
   var o = {
     username: false,
@@ -17,7 +17,55 @@ angular.module('songhop.services', [])
     } else {
       authRoute = 'login'
     }
-    return $http.post(SERVER.url + '/' + authRoute, {username: username});
+    return $http.post(SERVER.url + '/' + authRoute, {username: username})
+      .success(function(data){
+        o.setSession(data.username, data.session_id, data.favorites);
+      });
+  }
+
+  // set session data
+  o.setSession = function(username, session_id, favorites) {
+    if (username) o.username = username;
+    if (session_id) o.session_id = session_id;
+    if (favorites) o.favorites = favorites;
+
+    // set data in localstorage object
+    $localstorage.setObject('user', { username: username, session_id: session_id });
+  }
+
+  // check user has session (is logged in)
+  o.checkSession = function() {
+    var defer = $q.defer();
+
+    if (o.session_id) {
+      // if this session is already initialized in the service
+      defer.resolve(true);
+    } else {
+      // detect if there's a session in localstorage from previous use.
+      // if it is, pull into our service
+      var user = $localstorage.getObject('user');
+
+      if (user.username) {
+        // if there's a user, lets grab their favorites from the server
+        o.setSession(user.username, user.session_id);
+        o.populateFavorites().then(function() {
+          defer.resolve(true);
+        });
+      } else {
+        // no user info in localstorage, reject
+        defer.resolve(false);
+      }
+    }
+    return defer.promise;
+  }
+
+  // delete session data
+  o.destroySession = function() {
+    $localstorage.setObject('user', {});
+    o.username = false;
+    o.session_id = false;
+    o.favorites = [];
+    o.newFavorites = 0;
   }
 
   o.addSongToFavorites = function(song) {
